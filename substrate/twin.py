6597028aba9639b83eac73cd9c5bd72ff3856fb1
rawdaGastan <rody.rawda.rr@gmail.com>
2022-12-18 12:16:29 +0200

    test_twin

--- substrate/twin.py
+++ substrate/twin.py
@@ -4,6 +4,7 @@ from substrateinterface import SubstrateInterface
 from .identity import Identity
 import ipaddress
 
+
 class Twin_Info:
     """Twin info class"""
 
@@ -13,15 +14,16 @@ class Twin_Info:
         self.account_id = account_id
         self.ip = ip
         self.entities = entities
-        
+
+
 class Twin:
     """Twin class"""
-    
+
     def __init__(self, substrate: SubstrateInterface, identity: Identity):
         self.substrate = substrate
         self.identity = identity
         self.twin_info = None
-                       
+
     def get(self):
         """get the twin info for the account ID
 
@@ -29,8 +31,9 @@ class Twin:
             Twin_Info: the info for a twin
         """
         twin_id = get_twin_by_public_key(self.identity.public_key, self.substrate)
-        return get_twin_by_id(twin_id, self.substrate)
-    
+        if twin_id != None:
+            return get_twin_by_id(twin_id, self.substrate)
+
     def create(self, ip: str):
         """creates a new twin for account ID
 
@@ -42,12 +45,12 @@ class Twin:
         """
         call = self.substrate.compose_call("TfgridModule", "create_twin", {"ip": ip})
 
-        extrinsic = self.substrate.create_signed_extrinsic(call, self.identity.keypair)
+        extrinsic = self.substrate.create_signed_extrinsic(call, self.identity.key_pair)
         result = self.substrate.submit_extrinsic(extrinsic, True, True)
-        
-        if not result.is_success or result.error_message != None: 
+
+        if not result.is_success or result.error_message != None:
             raise Exception("creating a new twin failed with error: ", result.error_message)
-        
+
     def update(self, ip: str):
         """updates a twin with the ip
 
@@ -59,12 +62,12 @@ class Twin:
         """
         call = self.substrate.compose_call("TfgridModule", "update_twin", {"ip": ip})
 
-        extrinsic = self.substrate.create_signed_extrinsic(call, self.identity.keypair)
+        extrinsic = self.substrate.create_signed_extrinsic(call, self.identity.key_pair)
         result = self.substrate.submit_extrinsic(extrinsic, True, True)
-        
-        if not result.is_success or result.error_message != None: 
+
+        if not result.is_success or result.error_message != None:
             raise Exception("updating a new twin failed with error: ", result.error_message)
-                
+
     def accept_terms_and_conditions(self, document_link: str):
         """accepting terms and conditions
 
@@ -74,6 +77,11 @@ class Twin:
         Raises:
             Exception: accepting terms and conditions failed with error
         """
+
+        signed_terms = signed_terms_and_conditions(self.identity.public_key, self.substrate)
+        if signed_terms != None and len(signed_terms) > 0:
+            return
+
         document_hash = hashlib.md5(document_link.encode()).hexdigest()
 
         call = self.substrate.compose_call(
@@ -81,12 +89,13 @@ class Twin:
             "user_accept_tc",
             {"document_link": document_link, "document_hash": document_hash},
         )
-        
+
         extrinsic = self.substrate.create_signed_extrinsic(call, self.identity.key_pair)
         result = self.substrate.submit_extrinsic(extrinsic, True, True)
-        
-        if not result.is_success or result.error_message != None: 
+
+        if not result.is_success or result.error_message != None:
             raise Exception("accepting terms and conditions failed with error: ", result.error_message)
+
 
 def get_twin_by_id(id: int, substrate: SubstrateInterface):
     """get the twin info using ID
@@ -104,14 +113,15 @@ def get_twin_by_id(id: int, substrate: SubstrateInterface):
     twin = substrate.query("TfgridModule", "Twins", [id])
     if twin["id"].value == 0:
         raise Exception("twin not found")
-    
+
     version = twin["version"].value
     twin_id = twin["id"].value
     account_id = twin["account_id"].value
     ip = twin["ip"].value
     entities = twin["entities"].value
-    
+
     return Twin_Info(version, twin_id, account_id, ip, entities)
+
 
 def get_twin_by_public_key(public_key: bytes, substrate: SubstrateInterface):
     """get twin ID from a public key
@@ -124,16 +134,17 @@ def get_twin_by_public_key(public_key: bytes, substrate: SubstrateInterface):
     twin_id = substrate.query("TfgridModule", "TwinIdByAccountID", [public_key])
     if twin_id == 0:
         raise Exception("twin not found")
-    
+
     return twin_id
+
 
 def signed_terms_and_conditions(public_key: bytes, substrate: SubstrateInterface):
     """get the signed terms and conditions
-    
+
     Args:
         public_key (bytes): identity public key
         substrate (SubstrateInterface): substrate client
     """
-    
+
     terms_and_conditions = substrate.query("TfgridModule", "UsersTermsAndConditions", [public_key])
     return terms_and_conditions
--- test/substrate/twin_test.py
+++ test/substrate/twin_test.py
@@ -5,34 +5,69 @@ import logging
 from .utils import start_local_connection, ALICE_MNEMONICS, ALICE_ADDRESS
 from substrate import identity, twin
 
+substrate = start_local_connection()
+test_identity = identity.new_identity_from_sr25519_mnemonics(ALICE_MNEMONICS)
+test_twin = twin.Twin(substrate, test_identity)
 
-def test_get_twin_by_public_key():
-    """test get twin ID with a public key"""
+document_link = "someDocument"
+ip = ":::"
+test_twin_id = 0
 
-    substrate = start_local_connection()
 
+def test_accept_terms_and_conditions():
+    """test accept terms and conditions"""
     try:
-        twin.get_twin_by_public_key(ALICE_ADDRESS, substrate)
+        test_twin.accept_terms_and_conditions(document_link)
         assert True
     except Exception as exp:
         logging.exception(exp)
         assert False
 
+
+def test_create_twin():
+    """test create twin"""
+    try:
+        test_twin.create(ip)
+        assert True
+    except Exception as exp:
+        logging.exception(exp)
+        assert False
+
+
+def test_update_twin():
+    """test update twin"""
+    try:
+        test_twin.update(ip)
+        assert True
+    except Exception as exp:
+        logging.exception(exp)
+        assert False
+
+
+def test_get_twin():
+    """test get twin"""
+    twin_info = test_twin.get()
+    assert twin_info.id == test_twin_id
+
+
+def test_get_twin_by_public_key():
+    """test get twin ID with a public key"""
+
+    twin_id = twin.get_twin_by_public_key(ALICE_ADDRESS, substrate)
+    assert twin_id == test_twin_id
+
+
 def test_get_twin_by_id():
     """test get twin with ID"""
-        
-    substrate = start_local_connection()
-    test_twin = twin.get_twin_by_id(146, substrate)
-    
-    assert test_twin.id == 146
-    
-def test_get_twin():
-    """test get twin with ID"""
+    twin_info = twin.get_twin_by_id(test_twin_id, substrate)
+    assert twin_info.id == test_twin_id
 
-    substrate = start_local_connection()
-    test_identity = identity.new_identity_from_sr25519_mnemonics(ALICE_MNEMONICS)
-        
-    test_twin = twin.get_twin_by_id(146, substrate)
-    
-    assert test_twin.id == 146
-    
+
+def test_signed_terms_and_conditions():
+    """test signed terms and conditions"""
+    signed_terms_and_conditions = twin.signed_terms_and_conditions(ALICE_ADDRESS, substrate)
+
+    if signed_terms_and_conditions != None and len(signed_terms_and_conditions) > 0:
+        assert True
+    else:
+        assert False
