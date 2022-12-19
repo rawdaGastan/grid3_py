0a4c7727f1387b29b559c9cf2f95d5fbb83eab60
rawdaGastan <rody.rawda.rr@gmail.com>
2022-12-19 17:33:33 +0200

    deployment

--- substrate/exceptions.py
+++ substrate/exceptions.py
@@ -3,7 +3,17 @@ Exceptions module
 
 - TwinCreationException
 - TwinUpdateException
+
 - AccountActivationFailed
+
+- CapacityReservationContractCreationException
+- CapacityReservationContractUpdateException
+- NameCreationException
+- ContractCancelException
+
+- DeploymentCreationException
+- DeploymentUpdateException
+- DeploymentCancelException
 
 """
 
@@ -11,6 +21,18 @@ Exceptions module
 class GridException(Exception):
     def __init__(self, message, category=None, level=None, context=None):
         super().__init__(message)
+
+
+class DeploymentCreationException(GridException):
+    pass
+
+
+class DeploymentUpdateException(GridException):
+    pass
+
+
+class DeploymentCancelException(GridException):
+    pass
 
 
 class TwinCreationException(GridException):
@@ -26,4 +48,20 @@ class AccountActivationFailed(GridException):
 
 
 class AcceptingTermsAndConditionsFailed(GridException):
+    pass
+
+
+class CapacityReservationContractCreationException(GridException):
+    pass
+
+
+class CapacityReservationContractUpdateException(GridException):
+    pass
+
+
+class NameCreationException(GridException):
+    pass
+
+
+class ContractCancelException(GridException):
     pass
